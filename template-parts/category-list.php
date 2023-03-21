4a586f36efdf44e0db00dc758fae4ed015498aa9
Jonathan Grieve <mail@jonniegrieve.co.uk>
2023-03-21 14:48:52 +0000

    wp list categories in template part

--- template-parts/category-list.php
+++ template-parts/category-list.php
@@ -1,13 +1,13 @@
 <!-- add new categories --> 
+<?php $category_parameters = array(
+    "separator" => " - ", 
+    "title_li" => "Category List" 
+) ?>
 
-<?php echo "category-list.php"; ?>
-
-<span>Categories List => </span>
-
-<!--<a href="http://localhost/jgdm_blog/category/">Category Home</a> | <a href="http://localhost/jgdm_blog/category/news/">News</a> | <a href="http://localhost/jgdm_blog/category/hundred_days_code/">#100daysofcode</a> | <a href="http://localhost/jgdm_blog/category/jgdm_feature/">JGDM Feature</a> | <a href="http://localhost/jgdm_blog/category/learning_journal/">Learning Journal</a> | <a href="http://localhost/jgdm_blog/category/news/">News</a> | <a href="http://localhost/jgdm_blog/category/news_and_comment/">News and Comment</a> | <a href="http://localhost/jgdm_blog/category/webdesign_news_comment/">Web Design News &amp; Comment</a>-->
 
 <!-- Full list of categories -->
-<h3><?php 
-    wp_list_categories( " - "); 
-    //get_categories(); ?>
+<h3>
+    <?php 
+        wp_list_categories( $category_parameters ); 
+    ?>
 </h3> 
